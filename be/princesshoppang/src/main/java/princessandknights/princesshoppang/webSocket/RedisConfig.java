2e67ad9bddc96b497b6c5cbbd99727b4d66160d1
Chaeeun Lee <chaeni511@gmail.com>
2023-08-29 22:38:08 +0900

    [SETTING] add redis dependency

---
+++ be/princesshoppang/src/main/java/princessandknights/princesshoppang/webSocket/RedisConfig.java
@@ -0,0 +1,30 @@
+package princessandknights.princesshoppang.webSocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setUsePool(true);
+        jedisConnectionFactory.setHostName("3.35.111.230");
+        jedisConnectionFactory.setPort(6379);
+        jedisConnectionFactory.setPassword("mypass");
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
