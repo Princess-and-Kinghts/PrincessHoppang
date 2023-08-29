1b2e891fff001308e4a1bb3db2de50be37040043
Chaeeun Lee <chaeni511@gmail.com>
2023-08-29 10:26:14 +0900

    [SETTING] start project

---
+++ princesshoppang/src/main/java/princessandknights/princesshoppang/PrincesshoppangApplication.java
@@ -0,0 +1,13 @@
+package princessandknights.princesshoppang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincesshoppangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincesshoppangApplication.class, args);
+	}
+
+}
