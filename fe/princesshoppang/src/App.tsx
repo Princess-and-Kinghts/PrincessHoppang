b69408d9a193a70182ab1d7190e0f8407a41caa1
foresec <foresec@naver.com>
2023-08-29 19:09:16 +0900

    [SETTING] 간단한 라우터 추가

--- fe/princesshoppang/src/App.tsx
+++ fe/princesshoppang/src/App.tsx
@@ -1,15 +1,25 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/HomePage/Home";
+import Game from "./pages/GamePage/Game";
+import Chat from "./pages/ChatPage/Chat";
+import Community from "./pages/CommunityPage/Community";
+import Profile from "./pages/MyPage/Profile";
 
 function App() {
   return (
-    <>
-      <h1>공주호빵</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/mypage" element={<Profile />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 export default App;
---
+++ fe/princesshoppang/src/pages/GamePage/Game.tsx
@@ -0,0 +1,9 @@
+const Game = () => {
+  return (
+    <div>
+      <h1>Game</h1>
+    </div>
+  );
+};
+
+export default Game;
