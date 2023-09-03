94773078771f9d5ab1ea68d655c7663581717071
Chaeeun Lee <chaeni511@gmail.com>
2023-09-03 20:37:26 +0900

    [FEAT] user entity

---
+++ be/princesshoppang/src/main/java/princessandknights/princesshoppang/common/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package princessandknights.princesshoppang.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedDate
+//    @Column(updatable = false, nullable = false)
+    private LocalDateTime regDt;
+
+    @LastModifiedDate
+//    @Column(nullable = false)
+    private LocalDateTime modDt;
+}
+
