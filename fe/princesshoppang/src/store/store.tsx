fc49c1aa24a28eb8be1465ba1299b44dcdd6bcd5
Chaeeun Lee <chaeni511@gmail.com>
2023-09-05 23:14:28 +0900

    [SETTING] add axios

--- fe/princesshoppang/src/store/store.tsx
+++ fe/princesshoppang/src/store/store.tsx
@@ -1,7 +1,9 @@
 import { createStore, atom } from "jotai"
+// import { createStore, atom } from "jotai"
 // import { getNationType, getTokenStatusType } from "@/types/common/apiReturnTypes"
 
 export const mainStore = createStore()
+
 
 
 export{
