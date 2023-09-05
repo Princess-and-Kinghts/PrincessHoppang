b69a5fb62dcb1d7de2b60a9275684d58819cd239
Chaeeun Lee <chaeni511@gmail.com>
2023-09-05 22:41:29 +0900

    [TEST] add context path by @RequestMapping

--- be/princesshoppang/src/main/java/princessandknights/princesshoppang/test/TestController.java
+++ be/princesshoppang/src/main/java/princessandknights/princesshoppang/test/TestController.java
@@ -3,15 +3,13 @@ package princessandknights.princesshoppang.test;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
 @RequiredArgsConstructor
 @CrossOrigin(origins="*")
+@RequestMapping("/api")
 @Controller
 public class TestController {
     @GetMapping("/test")
