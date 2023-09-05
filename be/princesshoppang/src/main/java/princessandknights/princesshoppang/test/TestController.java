053a4e9ccb646c22a5614c40638ef89fc8d66db8
Chaeeun Lee <chaeni511@gmail.com>
2023-09-05 22:36:14 +0900

    [SETTING] delete spring security

--- be/princesshoppang/src/main/java/princessandknights/princesshoppang/test/TestController.java
+++ be/princesshoppang/src/main/java/princessandknights/princesshoppang/test/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
