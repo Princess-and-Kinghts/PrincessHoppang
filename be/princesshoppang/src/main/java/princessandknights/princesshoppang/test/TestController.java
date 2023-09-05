8aba8bae92e57661c800feb368ff9d20add66ca0
Chaeeun Lee <chaeni511@gmail.com>
2023-09-05 22:24:32 +0900

    [TEST] TestController

---
+++ be/princesshoppang/src/main/java/princessandknights/princesshoppang/test/TestController.java
@@ -0,0 +1,22 @@
+package princessandknights.princesshoppang.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@CrossOrigin(origins="*")
+@Controller
+public class TestController {
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        log.info("GET 테스트");
+        return "GET 테스트";
+    }
+}
